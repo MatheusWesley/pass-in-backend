3f5ca3dd907d23b57fd6f1e29041cdac61cfe4f3
MatheusWesley <matheus.tarra@gmail.com>
2024-04-04 21:22:21 -0300

    Aula 02 - Iniciando a aula 2 conhecendo mais sobre a integração do zod com o fastify e melhiras na tipagem dos dados

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
 import { PrismaClient } from "@prisma/client";
+import { generateSlug } from "./utils/generate-slug";
 
 const app = fastify();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
 const prisma = new PrismaClient({
   log: ['query']
 });
@@ -10,30 +15,48 @@ const prisma = new PrismaClient({
 
 // Definindo rotas
 app.get('/', () => {
-  return 'Pagina inicial'
+  return 'Em manutenção!!'
 })
 
-app.post('/events', async (request, reply) => {
+app.withTypeProvider<ZodTypeProvider>()
+  .post('/events', {
+    schema: {
+      body: z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable()
+      })
+    }
+  }, async (request, reply) => {
+    // Desestruturando o data.
+    const { title, details, maximumAttendees } = request.body
 
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable()
+    const slug = generateSlug(title)
+
+    // Verificando se ja existe um slug com o mesmo nome no banco
+    const eventWithSameSlug = await prisma.event.findUnique({
+      where: {
+        slug,
+      }
+    })
+
+    if (eventWithSameSlug !== null) {
+      throw new Error('Another event with same title already exists.')
+    }
+
+
+
+    const event = await prisma.event.create({
+      data: {
+        title,
+        details,
+        maximumAttendees,
+        slug,
+      },
+    })
+
+    return reply.status(201).send({ eventId: event.id })
   })
-
-  const data = createEventSchema.parse(request.body)
-
-  const event = await prisma.event.create({
-    data: {
-      title: data.title,
-      details: data.details,
-      maximumAttendees: data.maximumAttendees,
-      slug: new Date().toISOString()
-    },
-  })
-   
-  return reply.status(201).send({eventId: event.id})
-})
 
 // Definindo a porta de execução.
 app.listen({ port: 3333, host: '0.0.0.0'}).then(() => {
